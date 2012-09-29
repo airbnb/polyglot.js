504c21209f57a60eb48bfed416b97d86f02b899e
Spike Brehm <ocelot@gmail.com>
2012-09-28 17:07:20 -0700

    updating build/* to match latest source

--- build/polyglot.js
+++ build/polyglot.js
@@ -3,11 +3,39 @@
 //     polyglot.js may be freely distributed under the terms of the BSD
 //     license. For all licensing information, details, and documention:
 //     http://airbnb.github.com/polyglot.js
+//
+//
+// Polyglot.js is an I18n helper library written in JavaScript, made to
+// work both in the browser and in Node. It provides a simple solution for
+// interpolation and pluralization, based off of Airbnb's
+// experience adding I18n functionality to its Backbone.js and Node apps.
+//
+// Polylglot is agnostic to your translation backend. It doesn't perform any
+// translation; it simply gives you a way to manage translated phrases from
+// your client- or server-side JavaScript application.
+//
+
+//
 
 !function(root) {
   'use strict';
 
   var phrases = {};
+
+// ## Public Methods
+
+// ### Polyglot.extend(phrases)
+//
+// Use `extend` to tell Polyglot how to translate a given key.
+//
+//     Polyglot.extend({
+//       "hello": "Hello",
+//       "hello_name": "Hello, %{name}"
+//     });
+//
+// The key can be any string.  Feel free to call `extend` multiple times;
+// it will override any phrases with the same key, but leave existing phrases
+// untouched.
 
   function extend(morePhrases) {
     for (var key in morePhrases) {
@@ -15,13 +43,59 @@
         phrases[key] = morePhrases[key];
       }
     }
-  };
+  }
 
+// ### Polyglot.clear()
+//
+// Clears all phrases. Useful for special cases, such as freeing
+// up memory if you have lots of phrases but no longer need to
+// perform any translation. Also used internally by `replace`.
+
+  function clear() {
+    phrases = {};
+  }
+
+// ### Polyglot.replace(phrases)
+//
+// Completely replace the existing phrases with a new set of phrases.
+// Normally, just use `extend` to add more phrases, but under certain
+// circumstances, you may want to make sure no old phrases are lying around.
+
+  function replace(newPhrases) {
+    clear();
+    extend(newPhrases);
+  }
+
+// ### Polyglot.t(key, interpolationOptions)
+//
+// The most-used method. Provide a key, and `t` will return the
+// phrase.
+//
+//     Polyglot.t("hello");
+//     => "Hello"
+//
+// The phrase value is provided first by a call to `Polyglot.extend()` or
+// `Polyglot.replace()`.
+//
+// Pass in an object as the second argument to perform interpolation.
+//
+//     Polyglot.t("hello_name", {name: "Spike"});
+//     => "Hello, Spike"
+//
+// If you like, you can provide a default value in case the phrase is missing.
+// Use the special option key "_" to specify a default.
+//
+//     Polyglot.t("i_like_to_write_in_language", {
+//       _: "I like to write in %{language}.",
+//       language: "JavaScript"
+//     });
+//     => "I like to write in JavaScript."
+//
 
   function t(key, options) {
     var result;
     options = options || {};
-    var phrase = phrases[key] || options['default'] || '';
+    var phrase = phrases[key] || options._ || '';
     if (phrase === '') {
       warn('Missing translation for key: "'+key+'"');
       result = key;
@@ -29,34 +103,91 @@
       result = interpolate(phrase, options);
     }
     return result;
-  };
+  }
 
+// ### Polyglot.pluralize(noun, count)
+//
+// Polyglot provides a very basic pattern for providing
+// pluralization based on a singular noun. Because varous languages
+// have different nominal forms for zero, one, and multiple, and because
+// the noun can be before or after the count, we have to be overly explicit
+// about the possibile phrases.
+//
+// For pluralizing "car", it assumes you have phrase keys of the form:
+//
+//     Polyglot.extend({
+//       "pluralize.car.zero": "%{count} cars",
+//       "pluralize.car.one":  "%{count} car",
+//       "pluralize.car.many": "%{count} cars"
+//     });
+//
+// `Polyglot.pluralize()` will choose the appropriate phrase based
+// on the provided count.
+//
+//     Polyglot.pluralize("car", 0);
+//     => "0 cars"
+//
+//     Polyglot.pluralize("car", 1);
+//     => "1 car"
+//
+//     Polyglot.pluralize("car", 2);
+//     => "2 cars"
+//
+// The second argument can be a `Number` or anything with a `length` property,
+// such as an `Array` or a `Backbone.Collection`.
+//
+//     var Cars = Backbone.Collection.extend({});
+//     var cars = new Cars;
+//
+//     Polyglot.pluralize("car", cars);
+//     => "0 cars"
+//
+//     cars.add({make: "Ford", model: "Fiesta"});
+//
+//     Polyglot.pluralize("car", cars);
+//     => "1 car"
+//
+//     cars.add({make: "Subaru", model: "Impreza"});
+//
+//     Polyglot.pluralize("car", cars);
+//     => "2 cars"
+//
 
-  function interpolate(phrase, options) {
-    for (var arg in options) {
-      if (arg !== 'default' && options.hasOwnProperty(arg)) {
-        phrase = phrase.replace(new RegExp('%\\{'+arg+'\\}', 'g'), options[arg]);
-      }
-    }
-    return phrase;
-  };
-
-
-  function pluralize(object, count) {
+  function pluralize(noun, count) {
     if (count != null && typeof count.length !== 'undefined') {
       count = count.length;
     }
     var key;
     if (count === 0) {
-      key = "pluralize." + object + ".zero";
+      key = "pluralize." + noun + ".zero";
     } else if (count === 1) {
-      key = "pluralize." + object + ".one";
+      key = "pluralize." + noun + ".one";
     } else {
-      key = "pluralize." + object + ".many";
+      key = "pluralize." + noun + ".many";
     }
     return t(key, {count: count});
-  };
+  }
 
+// ### Polyglot.registerHandlebars(Handlebars)
+//
+// Registers Polyglot's Handlebars helpers on a given
+// Handlebars context. This is automatically called if we find
+// a global `Handlebars` object, which makes use in the
+// browser a snap if Handlebars is included before Polyglot.
+// Otherwise, you can manually register the Handlebars helpers
+// by passing in a Handlebars conext, which is the primary Node
+// use case:
+//
+//     var Handlebars = require('handlebars');
+//     var Polyglot = require('polyglot');
+//
+//     console.log(Handlebars.helpers.t);
+//     // => undefined
+//
+//     Polyglot.registerHandlebars(Handlebars);
+//
+//     console.log(Handlebars.helpers.t);
+//     // => function(){...}
 
   function registerHandlebars(handlebars) {
     for (var key in handlebarsHelpers) {
@@ -64,43 +195,74 @@
         handlebars.registerHelper(key, handlebarsHelpers[key]);
       }
     }
-  };
+  }
 
-  // Handlebars helpers
-  // ==================
+// ## Private Methods
 
-  var handlebarsHelpers = {
-    t: function(key, options) {
-      return t(key, options.hash);
-    },
+// ### interpolate
+//
+// Does the dirty work. Creates a `RegExp` object for each
+// interpolation placeholder.
 
-    pluralize: function(object, block) {
-      return pluralize(object, block.hash.count);
+  function interpolate(phrase, options) {
+    for (var arg in options) {
+      if (arg !== '_' && options.hasOwnProperty(arg)) {
+        phrase = phrase.replace(new RegExp('%\\{'+arg+'\\}', 'g'), options[arg]);
+      }
     }
-  };
+    return phrase;
+  }
 
-
-  // Helper functions
-  // ===================
+// ### warn
+//
+// Provides a warning in the console if a phrase key is missing.
 
   function warn(message) {
     root.console && root.console.warn && root.console.warn('WARNING: ' + message);
   }
 
-  // Export methods
-  // ==============
+// ## Handlebars helpers
+
+  var handlebarsHelpers = {
+
+// ### t
+//
+//     <h1>{{t "hello_name" name=name}}</h1>
+//
+// gives:
+//
+//     <h1>Hello, DeNiro.</h1>
+
+    t: function(key, options) {
+      return t(key, options.hash);
+    },
+
+// ### pluralize
+//
+//     <p>{{pluralize "car" count=carCollection}}</p>
+//
+// gives:
+//
+//     <p>3 cars</p>
+
+    pluralize: function(object, block) {
+      return pluralize(object, block.hash.count);
+    }
+  }
+
+
+// ## Export public methods
 
   var Polyglot = {
     extend: extend,
+    replace: replace,
+    clear: clear,
     t: t,
-    interpolate: interpolate,
     pluralize: pluralize,
     registerHandlebars: registerHandlebars
   };
 
-  // Export for Node, attach to `window` for browser.
-  // ================================================
-
+// Export for Node, attach to `window` for browser.
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Polyglot;
   } else {
@@ -108,7 +270,10 @@
   }
 
 
-  // Register Handlebars helpers if Handlebars is found.
+// Register Handlebars helpers if Handlebars is found.
+// If not, you can manually register Handlebars helpers by
+// calling `Polyglot.registerHandlebars(Handlebars)`,
+// passing in your Handlebars context.
   if (root.Handlebars && root.Handlebars.registerHelper) {
     registerHandlebars(root.Handlebars);
   }
