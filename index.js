airbnb/polyglot.js

commit 4e6fcf80a4286f27cce632dcd39a99584a9cbd2f

[Performance] instead of creating a new regex for each token, use the function form of `replace`.
Adapted from and closes #19.

index.js
@@ -287,25 +287,14 @@ function choosePluralForm(text, locale, count) {
 // Does the dirty work. Creates a `RegExp` object for each
 // interpolation placeholder.
 var dollarRegex = /\$/g;
-var dollarBillsYall = '$$$$';
+var dollarBillsYall = '$$';
+var tokenRegex = /%\{(.*?)\}/g;
 function interpolate(phrase, options) {
-  /* eslint-disable no-param-reassign */
-  forEach(options, function (replacement, arg) {
-    if (arg !== '_') {
-      // Ensure replacement value is escaped to prevent special $-prefixed
-      // regex replace tokens. the "$$$$" is needed because each "$" needs to
-      // be escaped with "$" itself, and we need two in the resulting output.
-      if (typeof replacement === 'string') {
-        replacement = replace.call(options[arg], dollarRegex, dollarBillsYall);
-      }
-      // We create a new `RegExp` each time instead of using a more-efficient
-      // string replace so that the same argument can be replaced multiple times
-      // in the same phrase.
-      phrase = replace.call(phrase, new RegExp('%\\{' + arg + '\\}', 'g'), replacement);
-    }
+  return phrase.replace(tokenRegex, function (expression, argument) {
+    if (!Object.prototype.hasOwnProperty.call(options, argument)) { return ''; }
+    // Ensure replacement value is escaped to prevent special $-prefixed regex replace tokens.
+    return replace.call(options[argument], dollarRegex, dollarBillsYall);
   });
-  /* eslint-enable no-param-reassign */
-  return phrase;
 }
 
 module.exports = Polyglot;

commit ac16cc1f2ad2bbb40232d2e11716b8dd7e100648

[Fix] restore unintentional breaking change of missing substitutions being untouched.
Broken in 4e6fcf80a4286f27cce632dcd39a99584a9cbd2f.

index.js
@@ -143,8 +143,7 @@ function transformPhrase(phrase, substitutions, locale) {
 
   // Interpolate: Creates a `RegExp` object for each interpolation placeholder.
   result = replace.call(result, tokenRegex, function (expression, argument) {
-    if (!has(options, argument)) { return ''; }
-    if (options[argument] == null) { return expression; }
+    if (!has(options, argument) || options[argument] == null) { return expression; }
     // Ensure replacement value is escaped to prevent special $-prefixed regex replace tokens.
     return replace.call(options[argument], dollarRegex, dollarBillsYall);
   });

test/index.js
@@ -34,7 +34,7 @@ describe('t', function () {
   it('interpolates with missing substitutions', function () {
     expect(polyglot.t('hi_name_welcome_to_place', {
       place: undefined
-    })).to.equal('Hi, , welcome to %{place}!');
+    })).to.equal('Hi, %{name}, welcome to %{place}!');
   });
 
   it('interpolates the same placeholder multiple times', function () {
@@ -324,7 +324,7 @@ describe('transformPhrase', function () {
   });
 
   it('removes missing keys', function () {
-    expect(Polyglot.transformPhrase(simple, { name: 'Polyglot' })).to.equal('Polyglot is ');
+    expect(Polyglot.transformPhrase(simple, { name: 'Polyglot' })).to.equal('Polyglot is %{attribute}');
   });
 
   it('selects the correct plural form based on smart_count', function () {