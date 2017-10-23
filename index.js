bd8f789f15ea166ea1310d0d518ea6b900a6e57b
Oleg Topchiy <thedotedge@users.noreply.github.com>
2017-10-23 21:25:57 +0200

    add Ukrainian (uk) to list of plural types

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ var pluralTypeToLanguages = {
   chinese: ['id', 'ja', 'ko', 'lo', 'ms', 'th', 'tr', 'zh'],
   german: ['fa', 'da', 'de', 'en', 'es', 'fi', 'el', 'he', 'hu', 'it', 'nl', 'no', 'pt', 'sv'],
   french: ['fr', 'tl', 'pt-br'],
-  russian: ['hr', 'ru', 'lt'],
+  russian: ['hr', 'ru', 'lt', 'uk'],
   czech: ['cs', 'sk'],
   polish: ['pl'],
   icelandic: ['is']
