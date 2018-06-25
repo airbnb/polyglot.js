7376e335c6ba2ec8df740aeb0393a686ba395e1a
Norman Rusch <norman.rusch@moccu.com>
2018-06-25 16:00:38 +0200

    Define regexp for whitespace once

--- index.js
+++ index.js
@@ -37,8 +37,9 @@ function has(target, key) {
 
 function trim(str) {
   /* eslint-disable no-control-regex */
-  var leftWhitespace = /^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/;
-  var rightWhitespace = /[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+$/;
+  var whitespace = '[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+';
+  var leftWhitespace = new RegExp('^' + whitespace);
+  var rightWhitespace = new RegExp(whitespace + '$');
   /* eslint-enable no-control-regex */
   return str.replace(leftWhitespace, '').replace(rightWhitespace, '');
 }
