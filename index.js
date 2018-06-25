73bca32b0212254883afac5944d8746645d30581
Norman Rusch <norman.rusch@moccu.com>
2018-06-25 15:25:06 +0200

    Remove 'string.prototype.trim' dependency

--- index.js
+++ index.js
@@ -31,7 +31,13 @@ var warning = require('warning');
 var has = function (target, key) {
   return key in target;
 };
-var trim = require('string.prototype.trim');
+var trim = function (str) {
+  /* eslint-disable no-control-regex */
+  var leftWhitespace = /^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/;
+  var rightWhitespace = /[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+$/;
+  /* eslint-enable no-control-regex */
+  return str.replace(leftWhitespace, '').replace(rightWhitespace, '');
+};
 
 var warn = function warn(message) {
   warning(false, message);
